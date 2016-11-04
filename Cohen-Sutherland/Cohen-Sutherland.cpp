4254e8fde8b68253b15b7464c00e672b5eeb2794
xiong-fu <lhz566590@163.com>
2016-11-04 13:02:27 +0800

    注释修改

--- Cohen-Sutherland/Cohen-Sutherland.cpp
+++ Cohen-Sutherland/Cohen-Sutherland.cpp
@@ -8,6 +8,7 @@
 #include<GL/glut.h>
 using namespace std;
 
+//数据域
 class wcPt2D {
 public:
 	float x, y;
@@ -16,18 +17,48 @@ GLubyte encode(wcPt2D pt, wcPt2D winMin, wcPt2D winMax);
 void swapPts(wcPt2D *p1, wcPt2D * p2);
 void swapCodes(GLubyte * c1, GLubyte *c2);
 void lineClipCohSuth(wcPt2D winMin, wcPt2D winMax, wcPt2D p1, wcPt2D p2);
-
 //编码
 const GLint winLeftBitCode = 0x1;
 const GLint winRightBitCode = 0x2;
 const GLint winBottomBitCode = 0x4;
 const GLint winTopBitCode = 0x8;
+//判断是否在窗口内，窗口外或者部分窗口内
+inline GLint inside(GLint code);
+inline GLint reject(GLint code1, GLint code2);
+inline GLint accept(GLint code1, GLint code2);
 
+//函数定义
+//端点的编码
+GLubyte encode(wcPt2D pt, wcPt2D winMin, wcPt2D winMax);
+//交换点
+void swapPts(wcPt2D *p1, wcPt2D *p2);
+//交换编码
+void swapCodes(GLubyte *c1, GLubyte *c2);
+//核心，裁剪算法
+void lineClipCohSuth(wcPt2D winMin, wcPt2D winMax, wcPt2D p1, wcPt2D p2);
+//初始化函数
+void init(void);
+//键盘交互Enter和Esc
+void myKeyBoard(unsigned char key, int x, int y);
+//显示窗口和未裁剪的直线
+void display();
+
+//主函数
+void main(int argc, char* argv) {
+	glutInit(&argc, &argv);
+	glutInitWindowPosition(50, 100);        
+	glutInitWindowSize(600, 600);  
+	glutCreateWindow("CLIP_LINE");
+	glutKeyboardFunc(myKeyBoard);
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
 //判断是否在窗口内，窗口外或者部分窗口内
 inline GLint inside(GLint code) {
 	return GLint(!code);
 }
-inline GLint reject(GLint code1,GLint code2) {
+inline GLint reject(GLint code1, GLint code2) {
 	return GLint(code1 & code2);
 }
 inline GLint accept(GLint code1, GLint code2) {
@@ -45,10 +76,11 @@ GLubyte encode(wcPt2D pt, wcPt2D winMin, wcPt2D winMax) {
 	}
 	if (pt.y < winMin.y) {
 		code = code | winBottomBitCode;
-	}else
-	if (pt.y > winMax.y) {
-		code = code | winTopBitCode;
 	}
+	else
+		if (pt.y > winMax.y) {
+			code = code | winTopBitCode;
+		}
 	return (code);//此处括号应该为了强调返回的是编码。
 /*PS：书中的代码的风格可读性较差，因为else－if之间使用换行缩进导致想看清if-else之间的匹配比较困难
 并且if在只有一行代码的情况下直接换行,界限模糊，应该加上｛｝使得界限更加明显，尽管这样会多用两行.
@@ -85,16 +117,18 @@ void lineClipCohSuth(wcPt2D winMin, wcPt2D winMax, wcPt2D p1, wcPt2D p2) {
 			plotLine = true;
 		}
 		else if (reject(code1, code2)) {
-				done = true;
-			}
+			done = true;
+		}
 		else {
 			if (inside(code1)) {
 				swapPts(&p1, &p2);
 				swapCodes(&code1, &code2);
-			}else{/**/}//和上一个if匹配，表明if已经结束
+			}
+			else {/**/ }//和上一个if匹配，表明if已经结束
 			if (p2.x != p1.x) {
 				m = (p2.y - p1.y) / (p2.x - p1.x);//GLfloat m
-			}else{/**/ }
+			}
+			else {/**/ }
 			if (code1 & winLeftBitCode) {
 				p1.y += (winMin.x - p1.x) * m;
 				p1.x = winMin.x;
@@ -106,15 +140,18 @@ void lineClipCohSuth(wcPt2D winMin, wcPt2D winMax, wcPt2D p1, wcPt2D p2) {
 			else if (code1 & winBottomBitCode) {
 				if (p2.x != p1.x) {
 					p1.x += (winMin.y - p1.y) / m;
-				}else{/**/ }
+				}
+				else {/**/ }
 				p1.y = winMin.y;
 			}
 			else if (code1 & winTopBitCode) {
 				if (p2.x != p1.x) {
 					p1.x += (winMax.y - p1.y) / m;
-				}else{/**/ }
+				}
+				else {/**/ }
 				p1.y = winMax.y;
-			}else{/**/ }
+			}
+			else {/**/ }
 		}//else
 	}//while
 	if (plotLine) {
@@ -122,7 +159,8 @@ void lineClipCohSuth(wcPt2D winMin, wcPt2D winMax, wcPt2D p1, wcPt2D p2) {
 		glVertex2f(p1.x, p1.y);
 		glVertex2f(p2.x, p2.y);
 		glEnd();
-	}else{/**/ }
+	}
+	else {/**/ }
 }
 
 //初始化函数
@@ -168,16 +206,4 @@ void display()
 	glEnd();
 
 	glFlush();
-}
-
-
-void main(int argc, char* argv) {
-	glutInit(&argc, &argv);
-	glutInitWindowPosition(50, 100);        
-	glutInitWindowSize(600, 600);  
-	glutCreateWindow("CLIP_LINE");
-	glutKeyboardFunc(myKeyBoard);
-	init();
-	glutDisplayFunc(display);
-	glutMainLoop();
 }
